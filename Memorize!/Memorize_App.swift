8413980383e9ada2489dd8f168d59e4959714c7f
EaRu723 <y.andrearusso@gmail.com>
2023-11-11 20:57:23 -0500

    Initial Commit

---
+++ Memorize!/Memorize_App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_App.swift
+//  Memorize!
+//
+//  Created by Andrea Russo on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
